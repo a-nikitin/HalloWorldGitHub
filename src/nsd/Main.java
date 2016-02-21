00052ea022b0f08a22c609e5225af1cb1bf767f1
Aleksey Nikitin <nikitin@skilion.ru>
2016-02-21 21:01:09 +0300

    Initial commit

---
+++ src/nsd/Main.java
@@ -0,0 +1,10 @@
+package nsd;
+
+/**
+ * Created by Алексей Никитин on 21.02.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
